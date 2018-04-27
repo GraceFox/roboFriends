8803cb122db25ee49a5eff0feaaa8afffa816909
Grace Fox <grace.fox@xero.com>
2018-04-27 16:34:01 +1200

    refactor
    Added searchbox
    made a parent App class importing reacts Component

--- src/CardList.js
+++ src/CardList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    //in loops in react, items need a unique key, here we can use the index as the key={i}, also want it to not change so id would be best as 
     return (
         <div>
             {
